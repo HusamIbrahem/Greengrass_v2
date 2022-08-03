42f5a667a8542feda3024456cb2323a3ff195d3b
HusamIbrahem <husamib93@gmail.com>
2022-08-03 23:53:29 +0300

    add components

---
+++ update_gg2.sh
@@ -0,0 +1,4 @@
+sudo /greengrass/v2/bin/greengrass-cli deployment create \
+  --recipeDir /home/husam/gg2_components/recipes \
+  --artifactDir /home/husam/gg2_components/artifacts \
+  --merge "com.example.blinky=1.0.0"
